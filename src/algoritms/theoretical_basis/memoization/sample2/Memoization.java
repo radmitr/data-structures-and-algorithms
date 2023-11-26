6025bceac8d471cf081915881824a529e27b3a37
Dmitriy Radionov <dmitriy.radionov@gmail.com>
2023-11-27 00:30:51 +0300

    Add 'memoization' package

---
+++ src/algoritms/theoretical_basis/memoization/sample2/Memoization.java
@@ -0,0 +1,27 @@
+package algoritms.theoretical_basis.memoization.sample2;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.function.Function;
+
+public class Memoization {
+
+	public static <T, R> Function<T, R> getMemoizedFunction(Function<T, R> fun) {
+		class MemCache implements Function<T, R> {
+			private ConcurrentMap<T, R> cache = new ConcurrentHashMap<>();
+
+			@Override
+			public R apply(T t) {
+				R result = cache.get(t);
+				if (result != null) {
+					System.out.println("Cache: {" + t + "=" + result + "}");
+					return result;
+				}
+				result = fun.apply(t);
+				cache.put(t, result);
+				return result;
+			}
+		}
+		return new MemCache();
+	}
+}
